c6e20f8eaa81715a038136c76f6d26ea3de0cee1
SantoshHrushith <hrushith2004@gmail.com>
2024-12-16 19:48:50 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
         <p><b>Skills:</b> C++, Python, Javascript, SQL, HTML, CSS</p>
         <p><b>Frame works :</b>Numpy, Tensorflow, Pandas, Scikit-learn, NodeJs, ExpressJs, ReactJs </p>
         <p><b>Experience:</b> Full Stack Web developer intern at Visakhapatnam Port Authority(June 2024 - July 2024)</p>
-        <div className='cv-ab'><a href="SantoshHrushith_C.pdf" download="SantoshHrushith_CV.pdf">Download CV</a></div>
+        <div className='cv-ab'><a href="SantoshHrushith.pdf" download="SantoshHrushith_CV.pdf">Download CV</a></div>
       </div>
     </div>
   )
