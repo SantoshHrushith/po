6de5b5c99fb07a71f046bb700b87c2242dc667c2
SantoshHrushith <hrushith2004@gmail.com>
2024-12-20 00:39:11 +0530

    Added Links to project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,27 +14,33 @@ const Projects = () => {
       <div className="project-container">
         <div className="project-card">
           <img src={p4} alt="Asset Management using MERN Stack" />
-          <div className="project-title">Asset Management using MERN Stack</div>
+          <div className="project-title"><a href='https://github.com/SantoshHrushith/Asset-Managment'>Asset Management using MERN Stack</a></div>
         </div>
-        
+
 
         <div className="project-card">
           <img src={p3} alt="Unitrade: A platform to buy and sell goods" />
           <div className="project-title">
-            Unitrade: A platform to buy and sell goods
+            <a href='https://github.com/SantoshHrushith/Unitrade'>
+              Unitrade: A platform to buy and sell goods
+            </a>
           </div>
         </div>
 
         <div className="project-card">
           <img src={p1} alt="Tomato Leaf Disease Detection using CNN" />
           <div className="project-title">
-            Tomato Leaf Disease Detection using CNN
+            <a href='https://github.com/SantoshHrushith/Unitrade'>
+              Tomato Leaf Disease Detection using CNN
+            </a>
           </div>
         </div>
 
         <div className="project-card">
           <img src={p2} alt="Email Spam Classification" />
-          <div className="project-title">Email Spam Classification</div>
+          
+            <div className="project-title"><a href='https://github.com/SantoshHrushith/Unitrade'>Email Spam Classification</a></div>
+          
         </div>
       </div>
 
